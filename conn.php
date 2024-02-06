d931306d1a497a7a004be0da231b24bb75c7c422
Tricciardone <125584144+Tricciardone@users.noreply.github.com>
2024-02-06 19:30:43 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("localhost","root","Walknico%10","chat");
+$conn = mysqli_connect("localhost","root","","chat");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
